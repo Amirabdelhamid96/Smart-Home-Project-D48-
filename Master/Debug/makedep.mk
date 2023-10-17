4c840564ad247b5a748f4c5f08b4d06c67addc6c
Amirabdelhamid96 <144169252+Amirabdelhamid96@users.noreply.github.com>
2023-10-17 16:21:24 +0300

    Add files via upload

---
+++ Master/Debug/makedep.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\ADC\ADC.c
+
+HAL\Bulb\Bulb.c
+
+HAL\Buzzer\Buzzer.c
+
+HAL\DC Motor\DC Motor.c
+
+HAL\Dimmer\Dimmer.c
+
+HAL\Interrupt\Interrupt.c
+
+HAL\keypad\keypad.c
+
+HAL\LCD\LCD.c
+
+HAL\Servo\Servo.c
+
+HAL\SPI\SPI.c
+
+HAL\UART\UART.c
+
+main.c
+
+MCAL\Bluetooth_Menu\Bluetooth_Menu.c
+
+MCAL\Menu\Menu.c
+
+MCAL\User_Login\User_Login.c
+
---
+++ Master/HAL/Buzzer/Buzzer.c
@@ -0,0 +1,23 @@
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define setbit(reg,bit) reg|=(1<<bit)
+#define clearbit(reg,bit) reg&=~(1<<bit)
+#define toggelbit(reg,bit) reg^=(1<<bit)
+#define readbit(reg,bit) ((reg>>bit)&1)
+
+
+void Buzzer_on(void)
+{
+	//buzzer
+	setbit(DDRA,3);
+	setbit(PORTA,3);
+}
+
+void Buzzer_off(void)
+{
+	//buzzer
+	setbit(DDRA,3);
+	clearbit(PORTA,3);
+}
